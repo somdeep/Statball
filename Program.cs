5c2d241cd5e36ca6c01c284aee2d08a11785b684
somdeep <sd2988@columbia.edu>
2021-06-03 09:28:58 +0530

    Added new stats for dm profile

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@
             StatReader statReader = new StatReader(fileArray);
 
             statReader.TopPlayers(statname: "Blocks_Int", count: 20, position: "MF", minimumFilter: 20, isp90: true);
-            statReader.SimilarPlayers(File.ReadAllLines("Profiles/DM.txt"), playerName: @"bissouma", count: 20, position: "MF", LeagueFilter: "", minimumFilter: 20, isp90: true);
+            statReader.SimilarPlayers(File.ReadAllLines("Profiles/DM.txt"), playerName: @"frenkie", count: 20, position: "MF", LeagueFilter: "", minimumFilter: 20, isp90: true);
         }
     }
 }
