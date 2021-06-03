6c3c47efb147545f09c172ada5f9da52b83b8871
somdeep <sd2988@columbia.edu>
2021-06-03 22:24:51 +0530

    Added player search

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
 
             statReader.TopPlayers(statname: "Blocks_Int", count: 30, position: "MF", minimumFilter: 20);
             statReader.SimilarPlayers(File.ReadAllLines("Profiles/CM.txt"), playerName: @"Grillitsch", count: 30, position: "MF", LeagueFilter: "", minimumFilter: 20);
-            statReader.ScoutPlayer(File.ReadAllLines("Profiles/DM.txt"), playerName: "Grillitsch", count: 30, position: "MF", LeagueFilter: "", TeamFilter: "", ageFilter: 30, minimumFilter: 20);
+            statReader.ScoutPlayer(File.ReadAllLines("Profiles/DM.txt"), playerName: "", count: 30, position: "MF", LeagueFilter: "", TeamFilter: "", ageFilter: 23, minimumFilter: 20);
         }
     }
 }
