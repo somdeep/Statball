3a3e522ab64eaa086e28fae2ee2ca23fc09108d9
somdeep <sd2988@columbia.edu>
2021-07-17 19:38:43 +0530

    Added maxstats resetter in statreader

--- StatReader.cs
+++ StatReader.cs
@@ -92,6 +92,8 @@ namespace Statball
 
         public void LoadMaxStats(double minimumFilter)
         {
+            maxStats = new Dictionary<string, double>();
+            
             foreach (var player in stats.Values)
             {
                 double _90s = Double.Parse(player["_90s"]);
