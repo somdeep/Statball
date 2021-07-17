f4ff2dd5c95c803ae7dfbb512867469e45566162
somdeep <sd2988@columbia.edu>
2021-07-17 14:55:23 +0530

    updated dm profile and generated new DM demo

--- Program.cs
+++ Program.cs
@@ -30,6 +30,7 @@
             statReader.TopPlayers(statname: "Blocks_Int", count: 30, position: "MF", minimumFilter: 20, outputFile: "Results/top_players.csv");
             statReader.SimilarPlayers(File.ReadAllLines("Profiles/CM.txt"), playerName: "camavinga", count: 30, position: "MF", LeagueFilter: "eng", minimumFilter: 20, outputFile: "Results/similar_players.csv");
             statReader.ScoutPlayer(File.ReadAllLines("Profiles/CM.txt"), playerName: "", count: 20, position: "MF", LeagueFilter: "eng", TeamFilter: "", ageFilter: 30, minimumFilter: 20, outputFile: "Results/scout_players.csv");
+            statReader.ScoutPlayer(File.ReadAllLines("Profiles/DM.txt"), playerName: "", count: 20, position: "MF", LeagueFilter: "eng", TeamFilter: "", ageFilter: 30, minimumFilter: 20, outputFile: "Results/Best_u30_DMs_EPL.csv");
             statReader.ScoutPlayer(File.ReadAllLines("Profiles/AM.txt"), playerName: "", count: 30, position: "MF", LeagueFilter: "", TeamFilter: "", ageFilter: 99, minimumFilter: 20, outputFile: "Results/Best_AMs.csv");
             statReader.ScoutPlayer(File.ReadAllLines("Profiles/AM.txt"), playerName: "", count: 30, position: "MF", LeagueFilter: "eng", TeamFilter: "", ageFilter: 99, minimumFilter: 20, outputFile: "Results/Best_AMs_EPL.csv");
 
