45df1bb8933380d16dc68dbf80be146701112c14
somdeep <sd2988@columbia.edu>
2021-07-17 12:40:24 +0530

    Added team scraping and refactored player scraping

--- Program.cs
+++ Program.cs
@@ -10,17 +10,21 @@
             string[] statNames = { "passing", "shooting", "passing_types", "gca", "defense", "possession", "misc", "stats" };
             // "playingtime", "keepers","keepersadv"};
 
-            string[] scrapedFiles = Directory.GetFiles(@"ScrapedResources");
+            Scraper scraper = new Scraper();
+
+            //Player Scraping and Scouting
+
+            string[] scrapedFiles = Directory.GetFiles(@"ScrapedPlayerResources");
             if (scrapedFiles.Length < statNames.Length)
             {
-                Scraper scraper = new Scraper();
+
                 foreach (string statname in statNames)
                 {
                     scraper.Scrape(statname);
                 }
             }
 
-            string[] fileArray = Directory.GetFiles(@"ScrapedResources");
+            string[] fileArray = Directory.GetFiles(@"ScrapedPlayerResources");
             StatReader statReader = new StatReader(fileArray, isp90: true);
 
             statReader.TopPlayers(statname: "Blocks_Int", count: 30, position: "MF", minimumFilter: 20, outputFile: "Results/top_players.csv");
@@ -28,6 +32,21 @@
             statReader.ScoutPlayer(File.ReadAllLines("Profiles/CM.txt"), playerName: "", count: 20, position: "MF", LeagueFilter: "eng", TeamFilter: "", ageFilter: 30, minimumFilter: 20, outputFile: "Results/scout_players.csv");
             statReader.ScoutPlayer(File.ReadAllLines("Profiles/AM.txt"), playerName: "", count: 30, position: "MF", LeagueFilter: "", TeamFilter: "", ageFilter: 99, minimumFilter: 20, outputFile: "Results/Best_AMs.csv");
             statReader.ScoutPlayer(File.ReadAllLines("Profiles/AM.txt"), playerName: "", count: 30, position: "MF", LeagueFilter: "eng", TeamFilter: "", ageFilter: 99, minimumFilter: 20, outputFile: "Results/Best_AMs_EPL.csv");
+
+
+            //Squad scraping and scouting
+
+            string[] scrapedSquadFiles = Directory.GetFiles(@"ScrapedSquadResources");
+            if (scrapedSquadFiles.Length < statNames.Length)
+            {
+
+                foreach (string statname in statNames)
+                {
+                    scraper.SquadScrape(statname);
+                }
+            }
+
+            
         }
     }
 }
--- Scraper.cs
+++ Scraper.cs
@@ -33,7 +33,7 @@ namespace Statball
             List<HtmlNodeCollection> playerNodes = doc.DocumentNode.SelectNodes($"//table[@id='stats_{statname}']//tbody//tr").Select(node => node.ChildNodes).ToList();
 
             int count = 0;
-            using (StreamWriter writer = new StreamWriter($"ScrapedResources/Player_{statname}.csv"))
+            using (StreamWriter writer = new StreamWriter($"ScrapedPlayerResources/Player_{statname}.csv"))
             {
                 foreach (string name in overHeaderNames)
                 {
@@ -77,6 +77,85 @@ namespace Statball
                             if (value == "position") writer.Write(node.InnerText.Replace(",", "") + ",");
                             else writer.Write(node.InnerText + ",");
                         }
+                    }
+
+                    count--;
+                    if (count > 2) writer.WriteLine();
+                }
+            }
+
+        }
+
+        public void SquadScrape(string statname = "passing")
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load($"https://fbref.com/en/comps/Big5/{statname}/squads/Big-5-European-Leagues-Stats");
+
+
+            if (statname == "stats") statname = "standard";
+
+            List<string> overHeaderNames = doc.DocumentNode.SelectNodes($"//table[@id='stats_squads_{statname}_for']//thead//tr")[0].ChildNodes.Where(n => n.Name == "th").Select(child => child.InnerText).ToList();
+
+            List<int> columnSpans = new List<int>();
+            var columns = doc.DocumentNode.SelectNodes($"//table[@id='stats_squads_{statname}_for']//thead//tr[@class='over_header']//th").Select(n => n.Attributes).ToList();
+            foreach (var column in columns)
+            {
+                int value = column.Any(c => c.Name == "colspan") ? Convert.ToInt32(column["colspan"].Value) : 1;
+                columnSpans.Add(value);
+            }
+
+            List<string> headerNames = doc.DocumentNode.SelectNodes($"//table[@id='stats_squads_{statname}_for']//thead//tr")[1].ChildNodes.Where(n => n.Name == "th").Select(child => child.InnerText).ToList();
+
+            List<HtmlNodeCollection> squadNodes = doc.DocumentNode.SelectNodes($"//table[@id='stats_squads_{statname}_for']//tbody//tr").Select(node => node.ChildNodes).ToList();
+
+            int count = 0;
+            using (StreamWriter writer = new StreamWriter($"ScrapedSquadResources/Squad_{statname}.csv"))
+            {
+                foreach (string name in overHeaderNames)
+                {
+                    int span = columnSpans[count++];
+                    writer.Write(name + ",");
+                    for (int i = 1; i < span; i++)
+                    {
+                        if (count == overHeaderNames.Count - 1 && i == span - 1) continue;
+
+                        writer.Write(",");
+                    }
+                }
+
+                writer.WriteLine();
+
+                count = headerNames.Count - 1;
+                foreach (string name in headerNames)
+                {
+                    if (count > 0) writer.Write(name + ",");
+                    else writer.Write(name);
+
+                    count--;
+                }
+
+                writer.WriteLine();
+                count = squadNodes.Count - 1;
+                foreach (var squadNode in squadNodes)
+                {
+                    string firstNode = squadNode.Nodes().First().InnerText;
+                    if (firstNode == "Rk") continue;
+
+                    int nodeCount = squadNode.Count - 1;
+                    foreach (var node in squadNode)
+                    {
+
+                        if (nodeCount == 0) writer.Write(node.InnerText);
+
+                        else
+                        {
+                            string value = node.Attributes.Where(n => n.Name == "data-stat").First().Value;
+
+                            if (value == "position") writer.Write(node.InnerText.Replace(",", "") + ",");
+                            else writer.Write(node.InnerText + ",");
+                        }
+
+                        nodeCount--;
                     }
 
                     count--;
